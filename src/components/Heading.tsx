3d6a50c81796f4d01642c72aca71c361e973a36f
Alexander Davis <alexander.davis.098@gmail.com>
2022-10-14 17:59:04 -0300

    Fix: Update components to accept classname props

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -6,20 +6,22 @@ export interface HeadingProps {
     size?: 'sm' | 'md' | 'lg';
     children: ReactNode;
     asChild?: boolean;
+    className?: string;
 }
 
-export function Heading({ size = 'md', children, asChild }: HeadingProps) {
+export function Heading({ size = 'md', children, asChild, className = 'text-gray-100' }: HeadingProps) {
     const Comp = asChild ? Slot : 'h2';
 
     return (
         <Comp 
             className={clsx(
-                'text-gray-100 font-bold font-sans', 
+                'font-bold font-sans', 
                 {
                     'text-lg': size === 'sm', // Apply class lg if the size is small
                     'text-xl': size === 'md', //Apply class xl if the size is medium                    
                     'text-2xl': size === 'lg' // Apply class 2xl if the size is large
-                }
+                },
+                className
             )}
         >
             {children}
